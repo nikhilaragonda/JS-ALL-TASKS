415e26c40f1d8160deaa54a26997f5a3aebcccfb
nikhilaragonda <nikhilaragonda44@gmail.com>
2024-11-12 15:30:03 +0530

    submit

--- TASK.13/js13.js
+++ TASK.13/js13.js
@@ -1,6 +1,18 @@
-1;
-var score = 50;
-// score >= 90
-//   ? console.log("Excellent"):score>=60 || score<=89?console.log("Good"):score<=60?console.log("Need improvement"):console.log("improve")
+var str = ["mom", "js", "dad", "malayalam", "html", "css"];
 
-score<=60?console.log("Need improvement"):console.log("improve")
+var str3 = [];
+for (i = 0; i < str.length; i++) {
+    var str1;
+  str1 = str[i];
+  var str2 = "";
+
+  for (j = str1.length - 1; j >= 0; j--) {
+    str2 += str1[j];
+  }
+
+if (str2 == str1) {
+
+    str3.push(str1)
+
+}}
+console.log(str3);
